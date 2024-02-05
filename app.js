87161fc63358b7ab93829308ffbdcf7690f070e7
aksdeo5 <akshaydeogaonkar2@gmail.com>
2024-02-05 12:33:56 +0530

    Object & Array Types | Type Aliases & Interfaces | Generics

--- app.js
+++ app.js
@@ -2,13 +2,24 @@
 const inputNum1 = document.getElementById("inputNum1");
 const inputNum2 = document.getElementById("inputNum2");
 const buttonAdd = document.querySelector("button");
+const numResults = [];
+const strResults = [];
 buttonAdd.addEventListener("click", (event) => {
     const num1 = inputNum1.value;
     const num2 = inputNum2.value;
-    console.log(add(+num1, +num2));
-    console.log(add(num1, num2));
-    console.log(add(+num1, num2));
+    const numResult = add(+num1, +num2);
+    const strResult = add(num1, num2);
+    numResults.push(numResult);
+    strResults.push(strResult);
+    console.log(numResult);
+    console.log(strResult);
+    printResult({ val: numResult, timestamp: new Date() });
+    console.log(numResults);
+    console.log(strResults);
 });
+function printResult(resultObj) {
+    console.log(resultObj.val);
+}
 function add(num1, num2) {
     if (typeof num1 === "number" && typeof num2 === "number")
         return num1 + num2;
@@ -17,3 +28,11 @@ function add(num1, num2) {
     else
         return +num1 + +num2;
 }
+const somePromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        resolve("It worked!");
+    }, 1000);
+});
+somePromise.then((result) => {
+    console.log(result.split(" "));
+});
--- app.ts
+++ app.ts
@@ -1,18 +1,49 @@
+type NumOrStr = number | string;
+type Result = { val: number; timestamp: Date };
+
+interface ResultI {
+	val: number;
+	timestamp: Date;
+}
+
 const inputNum1 = document.getElementById("inputNum1") as HTMLInputElement;
 const inputNum2 = document.getElementById("inputNum2") as HTMLInputElement;
 const buttonAdd = document.querySelector("button")!;
 
+const numResults: Array<number> = [];
+const strResults: string[] = [];
+
 buttonAdd.addEventListener("click", (event) => {
 	const num1 = inputNum1.value;
 	const num2 = inputNum2.value;
-	console.log(add(+num1, +num2));
-	console.log(add(num1, num2));
-	console.log(add(+num1, num2));
+	const numResult = add(+num1, +num2);
+	const strResult = add(num1, num2);
+	numResults.push(numResult as number);
+	strResults.push(strResult as string);
+	console.log(numResult);
+	console.log(strResult);
+	printResult({ val: numResult as number, timestamp: new Date() });
+	console.log(numResults);
+	console.log(strResults);
 });
 
-function add(num1: number | string, num2: number | string) {
+function printResult(resultObj: ResultI) {
+	console.log(resultObj.val);
+}
+
+function add(num1: NumOrStr, num2: NumOrStr) {
 	if (typeof num1 === "number" && typeof num2 === "number") return num1 + num2;
 	else if (typeof num1 === "string" && typeof num2 === "string")
 		return num1 + num2;
 	else return +num1 + +num2;
 }
+
+const somePromise = new Promise<string>((resolve, reject) => {
+	setTimeout(() => {
+		resolve("It worked!");
+	}, 1000);
+});
+
+somePromise.then((result) => {
+	console.log(result.split(" "));
+});
