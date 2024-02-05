ca167eb7754777a477390723758ce6b327f8c64c
aksdeo5 <akshaydeogaonkar2@gmail.com>
2024-02-05 08:52:31 +0530

    Type Inference & Type Casting | Configuring TypeScript | Union Types

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
+"use strict";
+const inputNum1 = document.getElementById("inputNum1");
+const inputNum2 = document.getElementById("inputNum2");
+const buttonAdd = document.querySelector("button");
+buttonAdd.addEventListener("click", (event) => {
+    const num1 = inputNum1.value;
+    const num2 = inputNum2.value;
+    console.log(add(+num1, +num2));
+    console.log(add(num1, num2));
+    console.log(add(+num1, num2));
+});
 function add(num1, num2) {
-    return num1 + num2;
+    if (typeof num1 === "number" && typeof num2 === "number")
+        return num1 + num2;
+    else if (typeof num1 === "string" && typeof num2 === "string")
+        return num1 + num2;
+    else
+        return +num1 + +num2;
 }
-console.log(add(1, 6));
-console.log(add("1", "6"));
